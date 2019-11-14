d9d5b5c46fd63c2cd2c534935a3291b8b58c3767
Sharon Stromberg <sharon@ccharacter.com>
2019-11-13 19:57:35 -0500

    v1.73

--- sws-wp-tweaks.php
+++ sws-wp-tweaks.php
@@ -4,7 +4,7 @@
  * Plugin Name:       SWS WordPress Tweaks
  * Plugin URI:        https://ccharacter.com/custom-plugins/sws-wp-tweaks/
  * Description:       Various tweaks that I'll want on most or all of my WordPress sites
- * Version:           1.7
+ * Version:           1.73
  * Requires at least: 5.2
  * Requires PHP:      5.5
  * Author:            Sharon Stromberg
