51df3dcfe4f1cf22bf0cf181822d191b88e4b4d7
Sharon Stromberg <sharon@ccharacter.com>
2020-02-19 08:18:15 -0500

    v2.10

--- options_page.php
+++ options_page.php
@@ -1,4 +1,39 @@
 <?php 
+
+// ADD TO PLUGIN ENTRY
+function plugin_add_settings_link( $links ) {
+    $settings_link = '<a href="' . esc_url( get_admin_url(null, 'options-general.php?page=sws_wp_tweaks') ) . '">' . __( 'Settings' ) . '</a>';
+    array_push( $links, $settings_link );
+  	return $links;
+}
+$plugin = plugin_basename( __FILE__ );
+add_filter( "plugin_action_links_$plugin", 'plugin_add_settings_link' );
+
+// ADD TO SETTINGS
+function sws_wp_tweaks_register_options_page() {
+  add_options_page('Sharon\'s WordPress Tweaks', 'SWS WP Tweaks', 'manage_options', 'sws_wp_tweaks', 'sws_wp_tweaks_options_page_html');
+}
+add_action('admin_menu', 'sws_wp_tweaks_register_options_page');
+
+
+// ADD TO TOP-LEVEL MENU
+function sws_wp_tweaks_options_page() {
+ // add top level menu page
+ add_menu_page(
+ 'SWS WP Tweaks TESST',
+ 'SWS WP Tweaks Options TEST',
+ 'manage_options',
+ 'sws_wp_tweaks',
+ 'sws_wp_tweaks_options_page_html'
+ );
+}
+ 
+/**
+ * register our wporg_options_page to the admin_menu action hook
+ */
+//add_action( 'admin_menu', 'sws_wp_tweaks_options_page' );
+
+
 // OPTIONS PAGE CODE
 function sws_wp_tweaks_register_settings() {
 
@@ -8,26 +43,136 @@ function sws_wp_tweaks_register_settings() {
 	 // register a new section in the "wporg" page
 	 add_settings_section(
 	 'sws_wp_tweaks_section_developers',
-	 __( 'The Matrix has you.', 'sws_wp_tweaks' ),
+	 __( 'Customize your options:', 'sws_wp_tweaks' ),
 	 'sws_wp_tweaks_section_developers_cb',
 	 'sws_wp_tweaks'
+	 );
+
+	 // register a new field in the "wporg_section_developers" section, inside the "wporg" page
+	 add_settings_field(
+	 'show_server_name', // as of WP 4.6 this value is used only internally
+	 // use $args' label_for to populate the id inside the callback
+	 __( 'Show server name*', 'sws_wp_tweaks' ),
+	 'sws_wp_tweaks_field_main_cb',
+	 'sws_wp_tweaks',
+	 'sws_wp_tweaks_section_developers',
+	 [
+	 'label_for' => 'show_server_name',
+	 'class' => 'sws_wp_tweaks_row',
+	 'sws_wp_tweaks_custom_data' => 'custom',
+	 ]
 	 );
 	 
 	 // register a new field in the "wporg_section_developers" section, inside the "wporg" page
 	 add_settings_field(
-	 'sws_wp_tweaks_field_pill', // as of WP 4.6 this value is used only internally
+	 'fix_its_fontpath', // as of WP 4.6 this value is used only internally
 	 // use $args' label_for to populate the id inside the callback
-	 __( 'Pill', 'sws_wp_tweaks' ),
-	 'sws_wp_tweaks_field_pill_cb',
+	 __( 'iThemes Security Font Path Bug', 'sws_wp_tweaks' ),
+	 'sws_wp_tweaks_field_main_cb',
 	 'sws_wp_tweaks',
 	 'sws_wp_tweaks_section_developers',
 	 [
-	 'label_for' => 'sws_wp_tweaks_field_pill',
+	 'label_for' => 'fix_its_fontpath',
 	 'class' => 'sws_wp_tweaks_row',
 	 'sws_wp_tweaks_custom_data' => 'custom',
 	 ]
 	 );
 
+	 // register a new field in the "wporg_section_developers" section, inside the "wporg" page
+	 add_settings_field(
+	 'fix_pw_reset_msg', // as of WP 4.6 this value is used only internally
+	 // use $args' label_for to populate the id inside the callback
+	 __( 'Fix Password Reset link', 'sws_wp_tweaks' ),
+	 'sws_wp_tweaks_field_main_cb',
+	 'sws_wp_tweaks',
+	 'sws_wp_tweaks_section_developers',
+	 [
+	 'label_for' => 'fix_pw_reset_msg',
+	 'class' => 'sws_wp_tweaks_row',
+	 'sws_wp_tweaks_custom_data' => 'custom',
+	 ]
+	 );
+
+	 // register a new field in the "wporg_section_developers" section, inside the "wporg" page
+	 add_settings_field(
+	 'disable_newUser_notice', // as of WP 4.6 this value is used only internally
+	 // use $args' label_for to populate the id inside the callback
+	 __( 'Disable notifications of new users', 'sws_wp_tweaks' ),
+	 'sws_wp_tweaks_field_main_cb',
+	 'sws_wp_tweaks',
+	 'sws_wp_tweaks_section_developers',
+	 [
+	 'label_for' => 'disable_newUser_notice',
+	 'class' => 'sws_wp_tweaks_row',
+	 'sws_wp_tweaks_custom_data' => 'custom',
+	 ]
+	 );
+	 
+	 
+	 	 // register a new field in the "wporg_section_developers" section, inside the "wporg" page
+	 add_settings_field(
+	 'disable_pwChange_notice', // as of WP 4.6 this value is used only internally
+	 // use $args' label_for to populate the id inside the callback
+	 __( 'Disable notifications of password changes', 'sws_wp_tweaks' ),
+	 'sws_wp_tweaks_field_main_cb',
+	 'sws_wp_tweaks',
+	 'sws_wp_tweaks_section_developers',
+	 [
+	 'label_for' => 'disable_pwChange_notice',
+	 'class' => 'sws_wp_tweaks_row',
+	 'sws_wp_tweaks_custom_data' => 'custom',
+	 ]
+	 );
+	 
+	 
+	 	 // register a new field in the "wporg_section_developers" section, inside the "wporg" page
+	 add_settings_field(
+	 'disable_xmlrpc', // as of WP 4.6 this value is used only internally
+	 // use $args' label_for to populate the id inside the callback
+	 __( 'Disable XML-RPC', 'sws_wp_tweaks' ),
+	 'sws_wp_tweaks_field_main_cb',
+	 'sws_wp_tweaks',
+	 'sws_wp_tweaks_section_developers',
+	 [
+	 'label_for' => 'disable_xmlrpc',
+	 'class' => 'sws_wp_tweaks_row',
+	 'sws_wp_tweaks_custom_data' => 'custom',
+	 ]
+	 );
+	 
+	 
+	 // register a new field in the "wporg_section_developers" section, inside the "wporg" page
+	 add_settings_field(
+	 'screen_grav_forms', // as of WP 4.6 this value is used only internally
+	 // use $args' label_for to populate the id inside the callback
+	 __( 'Gravity Form Screener', 'sws_wp_tweaks' ),
+	 'sws_wp_tweaks_field_main_cb',
+	 'sws_wp_tweaks',
+	 'sws_wp_tweaks_section_developers',
+	 [
+	 'label_for' => 'screen_grav_forms',
+	 'class' => 'sws_wp_tweaks_row',
+	 'sws_wp_tweaks_custom_data' => 'custom',
+	 ]
+	 );
+	 
+	 
+	 	 
+	 // register a new field in the "wporg_section_developers" section, inside the "wporg" page
+	 add_settings_field(
+	 'screen_form_ids', // as of WP 4.6 this value is used only internally
+	 // use $args' label_for to populate the id inside the callback
+	 __( 'Form ID(s) ~ separate with comma', 'sws_wp_tweaks' ),
+	 'sws_wp_tweaks_field_txt_cb',
+	 'sws_wp_tweaks',
+	 'sws_wp_tweaks_section_developers',
+	 [
+	 'label_for' => 'screen_form_ids',
+	 'class' => 'sws_wp_tweaks_row',
+	 'sws_wp_tweaks_custom_data' => 'custom',
+	 ]
+	 );
+	 
 }
 add_action( 'admin_init', 'sws_wp_tweaks_register_settings' );
 
@@ -35,9 +180,9 @@ add_action( 'admin_init', 'sws_wp_tweaks_register_settings' );
 // section callbacks can accept an $args parameter, which is an array.
 // $args have the following keys defined: title, id, callback.
 // the values are defined at the add_settings_section() function.
-function wporg_section_developers_cb( $args ) {
+function sws_wp_tweaks_section_developers_cb( $args ) {
  ?>
- <p id="<?php echo esc_attr( $args['id'] ); ?>"><?php esc_html_e( 'Follow the white rabbit.', 'wporg' ); ?></p>
+ <p id="<?php echo esc_attr( $args['id'] ); ?>"><?php esc_html_e( '* Appends near-translucent <span> to content of Home & About pages', 'sws_wp_tweaks' ); ?></p>
  <?php
 }
  
@@ -49,34 +194,37 @@ function wporg_section_developers_cb( $args ) {
 // the "label_for" key value is used for the "for" attribute of the <label>.
 // the "class" key value is used for the "class" attribute of the <tr> containing the field.
 // you can add custom key value pairs to be used inside your callbacks.
-function sws_wp_tweaks_field_pill_cb( $args ) {
+function sws_wp_tweaks_field_main_cb( $args ) {
  // get the value of the setting we've registered with register_setting()
  $options = get_option( 'sws_wp_tweaks_options' );
  // output the field
  ?>
  <select id="<?php echo esc_attr( $args['label_for'] ); ?>"
  data-custom="<?php echo esc_attr( $args['sws_wp_tweaks_custom_data'] ); ?>"
- name="wporg_options[<?php echo esc_attr( $args['label_for'] ); ?>]"
+ name="sws_wp_tweaks_options[<?php echo esc_attr( $args['label_for'] ); ?>]"
  >
- <option value="red" <?php echo isset( $options[ $args['label_for'] ] ) ? ( selected( $options[ $args['label_for'] ], 'red', false ) ) : ( '' ); ?>>
- <?php esc_html_e( 'red pill', 'sws_wp_tweaks' ); ?>
+ <option value="on" <?php echo isset( $options[ $args['label_for'] ] ) ? ( selected( $options[ $args['label_for'] ], 'on', false ) ) : ( '' ); ?>>
+ <?php esc_html_e( 'ON', 'sws_wp_tweaks' ); ?>
  </option>
- <option value="blue" <?php echo isset( $options[ $args['label_for'] ] ) ? ( selected( $options[ $args['label_for'] ], 'blue', false ) ) : ( '' ); ?>>
- <?php esc_html_e( 'blue pill', 'sws_wp_tweaks' ); ?>
+ <option value="off" <?php echo isset( $options[ $args['label_for'] ] ) ? ( selected( $options[ $args['label_for'] ], 'off', false ) ) : ( '' ); ?>>
+ <?php esc_html_e( 'OFF', 'sws_wp_tweaks' ); ?>
  </option>
  </select>
- <p class="description">
- <?php esc_html_e( 'You take the blue pill and the story ends. You wake in your bed and you believe whatever you want to believe.', 'sws_wp_tweaks' ); ?>
- </p>
- <p class="description">
- <?php esc_html_e( 'You take the red pill and you stay in Wonderland and I show you how deep the rabbit-hole goes.', 'sws_wp_tweaks' ); ?>
- </p>
  <?php
 }
 
 
+function sws_wp_tweaks_field_txt_cb( $args ) {
+	//error_log(print_r($args,true),0);
+	$options = get_option('sws_wp_tweaks_options');
+	if (isset($options[$args['label_for']])) { $myVal=$options[$args['label_for']]; } else { $myVal=""; }
+	//error_log(print_r($options,true),0);
+	// output the field
+ ?>
+ <input type='text' name="sws_wp_tweaks_options[<?php echo esc_attr( $args['label_for'] ); ?>]"  id="<?php echo esc_attr( $args['label_for'] ); ?>" value='<?php echo  $myVal; ?>'  data-custom="<?php echo esc_attr( $args['sws_wp_tweaks_custom_data'] ); ?>"><?php
+}
 
-function sws_wp_tweaks_options_page()
+function sws_wp_tweaks_options_page_html()
 {
 
 	 // check user capabilities
@@ -90,35 +238,36 @@ function sws_wp_tweaks_options_page()
 	 // wordpress will add the "settings-updated" $_GET parameter to the url
 	 if ( isset( $_GET['settings-updated'] ) ) {
 	 // add settings saved message with the class of "updated"
-	 add_settings_error( 'wporg_messages', 'wporg_message', __( 'Settings Saved', 'wporg' ), 'updated' );
+	 add_settings_error( 'sws_wp_tweaks_messages', 'sws_wp_tweaks_message', __( 'Settings Saved', 'sws_wp_tweaks' ), 'updated' );
 	 }
 	 
 	 // show error/update messages
-	 settings_errors( 'wporg_messages' );
+	 settings_errors( 'sws_wp_tweaks_messages' );
 	 ?>
+	 <style>
+	 .sws_wp_tweaks_row td { 
+		padding: 8px !important;
+	 }
+	 .sws_wp_tweaks_row th {
+		 width: 40%;
+		 text-align: right;
+		 padding: 8px 25px 8px 25px !important;
+	 }
+	 </style>
 	 <div class="wrap">
 	 <h1><?php echo esc_html( get_admin_page_title() ); ?></h1>
 	 <form action="options.php" method="post">
 	 <?php
 	 // output security fields for the registered setting "wporg"
-	 settings_fields( 'wporg' );
+	 settings_fields( 'sws_wp_tweaks' );
 	 // output setting sections and their fields
 	 // (sections are registered for "wporg", each field is registered to a specific section)
-	 do_settings_sections( 'wporg' );
+	 do_settings_sections( 'sws_wp_tweaks' );
 	 // output save settings button
 	 submit_button( 'Save Settings' );
 	 ?>
 	 </form>
 	 </div>
 	 <?php
-
-
-
-
-
-
-
 }
-
-
 ?>
--- sws-wp-tweaks.php
+++ sws-wp-tweaks.php
@@ -20,11 +20,12 @@ require_once plugin_dir_path(__FILE__).'inc/plugin-update-checker/plugin-update-
 $myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
 	'https://raw.githubusercontent.com/ccharacter/sws-wp-tweaks/master/plugin.json',
 	__FILE__,
-	'sws-wp-tweaks'
+	'sws_wp_tweaks'
 );
 
-require_once plugin_dir_path(__FILE__).'inc/action_link.php';
-require_once plugin_dir_path(__FILE__).'inc/save_options.php';
+require_once plugin_dir_path(__FILE__).'options_page.php';
+
+$optVals = get_option( 'sws_wp_tweaks_options' );
 
 // FOR NON-MULTI-SITES
 /*@ini_set( 'upload_max_size' , '64M' );
@@ -40,83 +41,118 @@ memory_limit = 1500M
 ... then sudo systemctl restart php-fpm
 */
 
-// FIX BUG IN iThemes Security path
-if ( ! function_exists( 'it_icon_font_admin_enueue_scripts' ) ) {
-        function it_icon_font_admin_enueue_scripts() {
-		        $url=plugins_url();
+// ON BY DEFAULT
+if ((!(isset($optVals['fix_its_fontpath']))) || ($optVals['fix_its_fontpath']=="on")) {
+	// FIX BUG IN iThemes Security path
+	if ( ! function_exists( 'it_icon_font_admin_enueue_scripts' ) ) {
+			function it_icon_font_admin_enueue_scripts() {
+					$url=plugins_url();
 
-                if ( version_compare( $GLOBALS['wp_version'], '3.7.10', '>=' ) ) {
-                        $dir = str_replace( '\\', '/', dirname( __FILE__ ) );
+					if ( version_compare( $GLOBALS['wp_version'], '3.7.10', '>=' ) ) {
+							$dir = str_replace( '\\', '/', dirname( __FILE__ ) );
 
-                        $content_dir = rtrim( str_replace( '\\', '/', WP_CONTENT_DIR ), '/' );
-                        $abspath = rtrim( str_replace( '\\', '/', ABSPATH ), '/' );
+							$content_dir = rtrim( str_replace( '\\', '/', WP_CONTENT_DIR ), '/' );
+							$abspath = rtrim( str_replace( '\\', '/', ABSPATH ), '/' );
 
-                        if ( empty( $content_dir ) || ( 0 === strpos( $dir, $content_dir ) ) ) {
-                                $url = WP_CONTENT_URL . str_replace( '\\', '/', preg_replace( '/^' . preg_quote( $content_dir, '/' ) . '/', '', $dir ) );
-                        } else if ( empty( $abspath ) || ( 0 === strpos( $dir, $abspath ) ) ) {
-                                $url = get_option( 'siteurl' ) . str_replace( '\\', '/', preg_replace( '/^' . preg_quote( $abspath, '/' ) . '/', '', $dir ) );
-                        }
+							if ( empty( $content_dir ) || ( 0 === strpos( $dir, $content_dir ) ) ) {
+									$url = WP_CONTENT_URL . str_replace( '\\', '/', preg_replace( '/^' . preg_quote( $content_dir, '/' ) . '/', '', $dir ) );
+							} else if ( empty( $abspath ) || ( 0 === strpos( $dir, $abspath ) ) ) {
+									$url = get_option( 'siteurl' ) . str_replace( '\\', '/', preg_replace( '/^' . preg_quote( $abspath, '/' ) . '/', '', $dir ) );
+							}
 
-                        if ( empty( $url ) ) {
-                                $dir = realpath( $dir );
+							if ( empty( $url ) ) {
+									$dir = realpath( $dir );
 
-                                if ( empty( $content_dir ) || ( 0 === strpos( $dir, $content_dir ) ) ) {
-                                        $url = WP_CONTENT_URL . str_replace( '\\', '/', preg_replace( '/^' . preg_quote( $content_dir, '/' ) . '/', '', $dir ) );
-                                } else if ( empty( $abspath ) || ( 0 === strpos( $dir, $abspath ) ) ) {
-                                        $url = get_option( 'siteurl' ) . str_replace( '\\', '/', preg_replace( '/^' . preg_quote( $abspath, '/' ) . '/', '', $dir ) );
-                                }
-                        }
+									if ( empty( $content_dir ) || ( 0 === strpos( $dir, $content_dir ) ) ) {
+											$url = WP_CONTENT_URL . str_replace( '\\', '/', preg_replace( '/^' . preg_quote( $content_dir, '/' ) . '/', '', $dir ) );
+									} else if ( empty( $abspath ) || ( 0 === strpos( $dir, $abspath ) ) ) {
+											$url = get_option( 'siteurl' ) . str_replace( '\\', '/', preg_replace( '/^' . preg_quote( $abspath, '/' ) . '/', '', $dir ) );
+									}
+							}
 
-                        if ( is_ssl() ) {
-                                $url = preg_replace( '|^http://|', 'https://', $url );
-                        } else {
-                                $url = preg_replace( '|^https://|', 'http://', $url );
-                        }
+							if ( is_ssl() ) {
+									$url = preg_replace( '|^http://|', 'https://', $url );
+							} else {
+									$url = preg_replace( '|^https://|', 'http://', $url );
+							}
 
 
-                        wp_enqueue_style( 'ithemes-icon-font', "$url/better-wp-security/lib/icon-fonts/icon-fonts.css" );
-                }
-        }
-        add_action( 'admin_enqueue_scripts', 'it_icon_font_admin_enueue_scripts' );
+							wp_enqueue_style( 'ithemes-icon-font', "$url/better-wp-security/lib/icon-fonts/icon-fonts.css" );
+					}
+			}
+			add_action( 'admin_enqueue_scripts', 'it_icon_font_admin_enueue_scripts' );
+	}
 }
 
 
-// FIX  BUG IN PASSWORD RESET MSG
-add_filter("retrieve_password_message", "sws_custom_password_reset", 99, 4);
+// ON BY DEFAULT
+if ((!(isset($optVals['fix_pw_reset_msg']))) || ($optVals['fix_pw_reset_msg']=="on")) {
+	// FIX  BUG IN PASSWORD RESET MSG
+	add_filter("retrieve_password_message", "sws_custom_password_reset", 99, 4);
 
-function sws_custom_password_reset($message, $key, $user_login, $user_data )    {
-	$message = "Someone has requested a password reset for the following account:
-" . sprintf(__('%s'), $user_data->user_email) . "
-If this was a mistake, just ignore this email and nothing will happen.
-To reset your password, visit the following address:
-" . network_site_url("wp-login.php?action=rp&key=$key&login=" . rawurlencode($user_login), 'login') . "\r\n";
+	function sws_custom_password_reset($message, $key, $user_login, $user_data )    {
+		$message = "Someone has requested a password reset for the following account:
+	" . sprintf(__('%s'), $user_data->user_email) . "
+	If this was a mistake, just ignore this email and nothing will happen.
+	To reset your password, visit the following address:
+	" . network_site_url("wp-login.php?action=rp&key=$key&login=" . rawurlencode($user_login), 'login') . "\r\n";
 
-  	return $message;
-
+		return $message;
+	}
 }
 
-// DISABLE NEW USER NOTIFICATIONS
-if ( ! function_exists( 'wp_new_user_notification' ) ) :
-	function wp_new_user_notification( $user_id ) {}
-endif;
+// ON BY DEFAULT
+if ((!(isset($optVals['disable_newUser_notice']))) || ($optVals['disable_newUser_notice']=="on")) {
+	// DISABLE ADMIN default WordPress new user notification emails
+	if ( ! function_exists ( 'wp_new_user_notification' ) ) :
+		function wp_new_user_notification( $user_id, $deprecated = null, $notify = '' ) {
 
+			global $wpdb, $wp_hasher;
+			$user = get_userdata( $user_id );
 
-// DISABLE PASSWORD CHANGE NOTIFICATIONS
-if ( !function_exists( 'wp_password_change_notification' ) ) {
-    function wp_password_change_notification() {}
+			// The blogname option is escaped with esc_html on the way into the database in sanitize_option
+			// we want to reverse this for the plain text arena of emails.
+			$blogname = wp_specialchars_decode(get_option('blogname'), ENT_QUOTES);
+
+			// Generate something random for a password reset key.
+			$key = wp_generate_password( 20, false );
+
+			/** This action is documented in wp-login.php */
+			do_action( 'retrieve_password_key', $user->user_login, $key );
+
+			// Now insert the key, hashed, into the DB.
+			if ( empty( $wp_hasher ) ) {
+				$wp_hasher = new PasswordHash( 8, true );
+			}
+			$hashed = time() . ':' . $wp_hasher->HashPassword( $key );
+			$wpdb->update( $wpdb->users, array( 'user_activation_key' => $hashed ), array( 'user_login' => $user->user_login ) );
+
+			$switched_locale = switch_to_locale( get_user_locale( $user ) );
+
+			$message = "Thank you for registering on $blogname.
+			".sprintf(__('Your username is: %s'), $user->user_login) . '
+			To set your password, visit the following URL:
+			' . network_site_url("wp-login.php?action=rp&key=$key&login=" . rawurlencode($user->user_login), 'login') . "
+			
+			~The Friendly Script";
+
+			wp_mail($user->user_email, sprintf(__('[%s]: Your username and password info'), $blogname), $message);
+		}
+	endif;
 }
 
-// Disable use XML-RPC
-add_filter( 'xmlrpc_enabled', '__return_false' );
+// ON BY DEFAULT
+if ((!(isset($optVals['disable_xmlrpc']))) || ($optVals['disable_xmlrpc']=="on")) {
+	// Disable use XML-RPC
+	add_filter( 'xmlrpc_enabled', '__return_false' );
+}
 
-
-
-// GRAVITY FORMS VALIDATION: FAIL HTML & FOREIGN CHARS
 function sws_test_input($value) {
-	
 	//error_log($value,0);
+	// TEST RUSSIAN: Это образец символов на запрещенном языке.
+	// TEST CHINESE: 这是一种禁止语言字符的示例。
 	$test=""; $ret=true;
-	$testHTML=strip_tags($value);				
+	$testHTML=strip_tags($value,"<p><br><span><em><strong><li><ul>");				
 	$testLang=preg_replace('/[^\00-\255]+/u', '', $value);
 	if ($value!=$testHTML) { 
 		$test=" HTML is not allowed in this field."; 
@@ -126,30 +162,21 @@ function sws_test_input($value) {
 		$test= "Non-English characters are not allowed in this field."; 
 		//error_log("Non-English",0);
 	}
-	
-	
+		
 	if (!($test=="")) { $ret=false;}
 	$retArr=array($ret,$test);
 	return $retArr;
 }
 
-
-add_filter( 'gform_validation', 'custom_validation' );
-function custom_validation( $validation_result ) {
- 
+function sws_custom_validation( $validation_result ) {
 	$form = $validation_result['form'];
- 	
 	foreach( $form['fields'] as &$field ) {
-		
 		if (!($field->failed_validation)) {
-			
 			if (is_array($field->inputs)) { 
 				$value = $field->inputs;
 			} else {  
 				$value=rgpost("input_".$field->id, true);
 			}
-			
-			
 			if (is_array($value)) { 
 				$k=1;
 				foreach ($value as $item) {
@@ -172,13 +199,29 @@ function custom_validation( $validation_result ) {
 			}
 		} 
 	}
- 
-    //Assign modified $form object back to the validation result
-    $validation_result['form'] = $form;
-    return $validation_result;
- 
+	//Assign modified $form object back to the validation result
+	$validation_result['form'] = $form;
+	return $validation_result;
 }
+	
 
+// OFF BY DEFAULT
+if ((isset($optVals['screen_grav_forms'])) && ($optVals['screen_grav_forms']=="on")) {
+	// GRAVITY FORMS VALIDATION: FAIL HTML & FOREIGN CHARS
+	if (isset($optVals['screen_form_ids'])) { 
+		$idArr=explode(",",$optVals['screen_form_ids']);
+	}	
+	//error_log(print_r($idArr,true),0);
+
+	foreach ($idArr as $formID) {
+		if (strlen(trim($formID))>0) {
+			$formID=intval(trim ($formID));
+			
+			add_filter( 'gform_validation_'.$formID, 'sws_custom_validation' );
+			//error_log('gform_validation_'.$formID,0);
+		}
+	}
+}
 
 
 class DisplayServerName
@@ -203,10 +246,11 @@ class DisplayServerName
     }
 }
 
-
-$shortcode=new DisplayServerName();
-$shortcode->init();
-
+// ON BY DEFAULT
+if ((!(isset($optVals['show_server_name']))) || ($optVals['show_server_name']=="on")) {
+	$shortcode=new DisplayServerName();
+	$shortcode->init();
+}
 
 
 ?>
