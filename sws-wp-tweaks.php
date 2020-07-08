1e0d2bfbce3d5510339320b23768e93d5ac277f6
Sharon Stromberg <sharon@ccharacter.com>
2020-07-07 21:05:14 -0400

    v4.26
    mod github link for main branch, not master

--- sws-wp-tweaks.php
+++ sws-wp-tweaks.php
@@ -4,7 +4,7 @@
  * Plugin Name:       SWS WordPress Tweaks
  * Plugin URI:        https://ccharacter.com/custom-plugins/sws-wp-tweaks/
  * Description:       Various tweaks that I'll want on most or all of my WordPress sites
- * Version:           4.25
+ * Version:           4.26
  * Requires at least: 5.2
  * Requires PHP:      5.5
  * Author:            Sharon Stromberg
@@ -18,7 +18,7 @@ defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
 
 require_once plugin_dir_path(__FILE__).'inc/plugin-update-checker/plugin-update-checker.php';
 $myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
-	'https://raw.githubusercontent.com/ccharacter/sws-wp-tweaks/master/plugin.json',
+	'https://raw.githubusercontent.com/ccharacter/sws-wp-tweaks/main/plugin.json',
 	__FILE__,
 	'sws_wp_tweaks'
 );
