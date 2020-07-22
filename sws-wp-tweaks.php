e0898e391842f4dd86c6d3299f6d75fead8f0120
Sharon Stromberg <sharon@ccharacter.com>
2020-07-22 14:39:42 -0400

    ffffff

--- sws-wp-tweaks.php
+++ sws-wp-tweaks.php
@@ -253,13 +253,13 @@ if ((isset($optVals['delete_never_logged_in'])) && ($optVals['delete_never_logge
 		$query="SELECT `ID`,`user_login` FROM {$wpdb->prefix}users where `ID` not in (select uid from $tableName)"; error_log($query,0);
 		$delArr=$wpdb->get_results($query, ARRAY_A);
 		error_log(print_r($delArr,true),0);
-		/*foreach ($delArr as $row) { 
-			$thisID=$row['ID'];
-			$thisUser=get_user_by('id',$thisID);
+		foreach ($delArr as $row) { 
+			$thisID=$row['ID']; error_log($thisID,0);
+			/*$thisUser=get_user_by('id',$thisID);
 			if (!($thisUser->has_cap('manage_options'))) { 
 				error_log($thisID."|".$row['user_login']);
-			}
-		}*/
+			}*/
+		}
 	} else { 
 		sws_console_log("Simple Login Log does not exist."); 
 	}
