f12c562bfbbb36f6b5f693a4aeec7cc2674f1159
Sharon Stromberg <sharon@ccharacter.com>
2020-07-22 14:47:42 -0400

    fdsasdf

--- sws-wp-tweaks.php
+++ sws-wp-tweaks.php
@@ -255,7 +255,7 @@ if ((isset($optVals['delete_never_logged_in'])) && ($optVals['delete_never_logge
 		error_log(print_r($delArr,true),0);
 		foreach ($delArr as $row) { 
 			$thisID=$row['ID']; error_log($thisID,0);
-			$thisUser=get_user_by('id',intval($thisID));
+			$thisUser=get_user_by('id',2);
 			/*if (!($thisUser->has_cap('manage_options'))) { 
 				error_log($thisID."|".$row['user_login']);
 			}*/
