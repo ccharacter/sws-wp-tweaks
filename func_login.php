7b5c241628f2e231ff0be0bf35647d09da472688
Sharon Stromberg <sharon@ccharacter.com>
2020-07-27 16:30:32 -0400

    v4.41

--- func_login.php
+++ func_login.php
@@ -3,21 +3,25 @@
 // NOTE TO SELF: in iThemes Security->WordPress Tweaks, make sure login errors are showing
 
 // LOGIN FUNCTIONS
-function sws_tweaks_login_logo() { 
-	$optVals = get_option( 'sws_wp_tweaks_options' );
-	$logo_url=$optVals['login_logo'];
-?>
-    <style type="text/css">
-        #login h1 a, .login h1 a {
-        background-image: url('<?php echo $logo_url; ?>');
-        height:200px !important;
-        width:200px !important;
-        background-size: contain;
-        padding-bottom: -25px;
-        }
-    </style>
-<?php }
-add_action( 'login_enqueue_scripts', 'sws_tweaks_login_logo' );
+// OFF BY DEFAULT
+if (isset($optVals['login_logo']))  {
+	function sws_tweaks_login_logo() { 
+		$optVals = get_option( 'sws_wp_tweaks_options' );
+		$logo_url=$optVals['login_logo'];
+	?>
+		<style type="text/css">
+			#login h1 a, .login h1 a {
+			background-image: url('<?php echo $logo_url; ?>');
+			height:200px !important;
+			width:200px !important;
+			background-size: contain;
+			padding-bottom: -25px;
+			}
+		</style>
+	<?php }
+	add_action( 'login_enqueue_scripts', 'sws_tweaks_login_logo' );
+}
+
 
 function sws_tweaks_login_logo_url() {
 	return get_bloginfo( 'url' );
@@ -28,6 +32,16 @@ function sws_tweaks_login_head() {
 remove_action('login_head', 'wp_shake_js', 12);
 }
 add_action('login_head', 'sws_tweaks_login_head');
+
+function sws_tweaks_ck_old_banned () {
+	$extensions="https://docs.google.com/spreadsheets/d/e/2PACX-1vTMtU2xcD85-JGUC7YSyqA8gJSRE0S2jchGwx7pTEBM0Ctbwdtyfy6K0SGWc_3OxX7CRjeNyXYllAtQ/pub?output=csv";
+
+	$keywords="https://docs.google.com/spreadsheets/d/e/2PACX-1vTbMPp5ITCS8-jUzN4bECUu5st9BmQ-9mZEXrqQpW3O0tcHKrNbvAk_-0l5ecoqgHV3Wka3uwnFegkG/pub?output=csv"; // LOGIN BANNING
+
+	$extArr=sws_tweaks_csvToArray($extensions,',',"N");
+	$keyArr=sws_tweaks_csvToArray($keywords,',',"N");
+	
+}
 
 
 
