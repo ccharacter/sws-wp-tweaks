d48907a252712166a6365ad267669fa353096eb7
Sharon Stromberg <sharon@ccharacter.com>
2020-08-11 17:58:20 -0400

    try again

--- inc/sws_tweaks_post_count_script.js
+++ inc/sws_tweaks_post_count_script.js
@@ -3,7 +3,7 @@ jQuery(document).ready(function()
 {
 
 	if (jQuery('#sws-tweaks-tpost-0').length) {
-		alert(jQuery('#sws-tweaks-tpost-0').width());
-		jQuery('.sws-tweaks-tpost-img-div').min-height(jQuery('#sws-tweaks-tpost-0').width());
+		var myHeight = jQuery('#sws-tweaks-tpost-0').width();
+		jQuery('.sws-tweaks-tpost-img-div').height(myHeight);
 	}
 });
