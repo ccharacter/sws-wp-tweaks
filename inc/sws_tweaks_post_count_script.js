e5384b6e9ec794cee993a7407a675f0052309a31
Sharon Stromberg <sharon@ccharacter.com>
2020-08-11 17:54:47 -0400

    js alert

--- inc/sws_tweaks_post_count_script.js
+++ inc/sws_tweaks_post_count_script.js
@@ -3,7 +3,7 @@ jQuery(document).ready(function()
 {
 
 	if (jQuery('#sws-tweaks-tpost-0').length) {
-		//alert(jQuery('#sws-tweaks-tpost-0').width());
+		alert(jQuery('#sws-tweaks-tpost-0').width());
 		jQuery('.sws-tweaks-tpost-img-div').height(jQuery('#sws-tweaks-tpost-0').width());
 	}
 });
