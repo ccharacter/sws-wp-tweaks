c54b9a4a3f66390dab3555610693892f5df1744c
Sharon Stromberg <sharon@ccharacter.com>
2020-08-11 17:55:32 -0400

    min-height?

--- inc/sws_tweaks_post_count_script.js
+++ inc/sws_tweaks_post_count_script.js
@@ -4,6 +4,6 @@ jQuery(document).ready(function()
 
 	if (jQuery('#sws-tweaks-tpost-0').length) {
 		alert(jQuery('#sws-tweaks-tpost-0').width());
-		jQuery('.sws-tweaks-tpost-img-div').height(jQuery('#sws-tweaks-tpost-0').width());
+		jQuery('.sws-tweaks-tpost-img-div').min-height(jQuery('#sws-tweaks-tpost-0').width());
 	}
 });
