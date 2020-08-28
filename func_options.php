063e897d8c05bba5c5adc159d6a26a7a03480c50
Sharon Stromberg <sharon@ccharacter.com>
2020-08-28 18:07:07 -0400

    v4.7
    fix url screener vars

--- func_options.php
+++ func_options.php
@@ -13,7 +13,7 @@ function sws_tweaks_get_default_options() {
 		'disable_pwChange_notice' => 'on',
 		'disable_xmlrpc' => 'on',
 		'screen_grav_forms' => 'off',
-		'screen_url' => 'off',
+		'screen_urls' => 'off',
 		'screen_form_ids' => ''
     );
     return $options;
@@ -232,13 +232,13 @@ function sws_wp_tweaks_register_settings() {
 	 	 
 	 // register a new field 
 	 add_settings_field(
-	 'screen_url', 
+	 'screen_urls', 
 	 __( 'URL Screener', 'sws_wp_tweaks' ),
 	 'sws_wp_tweaks_field_main_cb',
 	 'sws_wp_tweaks',
 	 'sws_wp_tweaks_section_developers',
 	 [
-	 'label_for' => 'screen_url',
+	 'label_for' => 'screen_urls',
 	 'class' => 'sws_wp_tweaks_row',
 	 'sws_wp_tweaks_custom_data' => 'custom',
 	 ]
