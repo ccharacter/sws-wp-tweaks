3a86d77252e4e8ea13554ea8aa5049d2b61913eb
Sharon Stromberg <sharon@ccharacter.com>
2020-11-01 17:41:08 -0500

    didn't need classes.php

--- sws-wp-tweaks.php
+++ sws-wp-tweaks.php
@@ -17,8 +17,6 @@
 
 defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
 
-require_once plugin_dir_path(__FILE__).'inc/classes.php';
-
 require_once plugin_dir_path(__FILE__).'inc/plugin-update-checker/plugin-update-checker.php';
 $myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
 	'https://raw.githubusercontent.com/ccharacter/sws-wp-tweaks/main/plugin.json',
